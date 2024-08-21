1509b3947aa0dfd2e4281f06574c2d4e033acfb3
Jakub <jakub.zajac44@gmail.com>
2024-08-21 11:31:05 +0200

    Create model in data and domain layer Create mapper

--- feature/weather/src/main/java/com/jakub/zajac/feature/weather/data/remote/model/LocationDto.kt
+++ feature/weather/src/main/java/com/jakub/zajac/feature/weather/data/remote/model/LocationDto.kt
@@ -1,22 +1,12 @@
 package com.jakub.zajac.feature.weather.data.remote.model
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-data class LocationDto (
-    @SerialName("Key")
-    val key: String,
-    @SerialName("Type")
-    val type: String,
-    @SerialName("LocalizedName")
-    val name: String,
-    @SerialName("Country")
-    val countryDto: CountryDto
-
-)
-
-data class CountryDto(
-    @SerialName("ID")
-    val id: String,
-    @SerialName("LocalizedName")
-    val name: String
+@Serializable
+data class LocationDto(
+    @SerialName("Key") val key: String,
+    @SerialName("Type") val type: String,
+    @SerialName("LocalizedName") val name: String,
+    @SerialName("Country") val countryDto: CountryDto
 )
